ef866c3ed8303c396b78d489d736a50466e90494
Roushan Sourav <roushansourav@outlook.com>
2019-04-23 11:34:08 +0530

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 200px;
+        }
+        .body{
+            align-content:center;
+        }
+    </style>
+</head>
+<body class="body">
+    <h1 style="align-content:center">Login Page</h1>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td><asp:TextBox ID="TextBox1" runat="server" Width="248px"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><asp:TextBox ID="TextBox2" runat="server" Width="248px"></asp:TextBox></td>
+                </tr>
+                
+            </table>
+            
+            
+        </div>
+        
+        
+        <p class="auto-style1">
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Login" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;//to access sql server
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new
+                SqlConnection(@"initial catalog = ADOdemo ; data source = CGI-JAVA-055\CGI ;integrated security = true");
+
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from users", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "t1");
+            DataTable dt = ds.Tables["t1"];
+            string user = TextBox1.Text;
+            string pass = TextBox2.Text;
+            DataRow fr = dt.Rows[0];
+            bool login = false;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                fr = dt.Rows[i];
+                if (fr[0].ToString() == user && fr[1].ToString() == pass)
+                {
+                    login = true;
+                    break;
+                }
+            }
+            if(login)
+            {
+                Response.Write("Login Successfull");
+            }
+            else
+            {
+                Response.Write("Login Failed");
+            }
+        }
+    }
+}
